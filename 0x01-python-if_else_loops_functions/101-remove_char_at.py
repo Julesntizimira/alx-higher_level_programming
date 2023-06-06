110e0849e476c2a9d8339b21052add4ca2d306df
jules ntizimira <ntizimijules@gmail.com>
2023-06-06 21:43:42 +0000

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
     s = ""
     for i in str:
         if i == str[n]:
             continue
         s = s + i
     return s
+
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
