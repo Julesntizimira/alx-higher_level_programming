62ea1f620ebc4387daecfa0c6311f97b2545df60
jules ntizimira <ntizimijules@gmail.com>
2023-06-11 09:43:00 +0000

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,57 +1,49 @@
 # include "lists.h"
 /**
- * list_len - compute list len
- * @head: pointer to head of list
- * Return: list length
- */
-int list_len(listint_t *head)
-{
-	listint_t *tmp = head;
-	int count = 0;
-
-	while (tmp != NULL)
-	{
-		count++;
-		tmp = tmp->next;
-	}
-	return (count);
-}
-/**
  * is_palindrome - check for palindrome
  * @head: pointer to head of list
  * Return: 1 if the list is palindrome or 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head;
-	int len = 0,  i = 0, *temp;
-	int *ptr = NULL, *p;
+	listint_t *fast = *head, *tmp = NULL, *first = *head;
+	listint_t *slow = *head, *h = NULL, *prev = NULL;
 
 	if (*head == NULL || head == NULL)
 		return (1);
-	if (list_len(tmp) == 1)
+	if ((*head)->next == NULL)
 		return (1);
-	len = list_len(tmp);
-	ptr = malloc(len * sizeof(int));
-	if (ptr == NULL)
-		return (0);
-	for (i = 0; i < len; i++)
+	while (1)
 	{
-		ptr[i] = tmp->n;
+		tmp = slow;
+		if (fast == NULL)
+		{
+			tmp = tmp->next;
+			break;
+		}
+		else if (fast->next == NULL)
+		{
+			tmp = tmp->next->next;
+			break;
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	slow->next = NULL;
+	while (tmp != NULL)
+	{
+		h = tmp;
+		tmp = tmp->next;
+		h->next = prev;
+		prev = h;
+	}
+	tmp = h;
+	while (first && tmp)
+	{
+		if (first->n != tmp->n)
+			return (0);
+		first = first->next;
 		tmp = tmp->next;
 	}
-	temp = &ptr[len - 1];
-	p = &ptr[0];
-	while (p < temp)
-	{
-		if (*p != *temp)
-		{
-			free(ptr);
-			return (0);
-		}
-		p++;
-		temp--;
-	}
-	free(ptr);
 	return (1);
 }
