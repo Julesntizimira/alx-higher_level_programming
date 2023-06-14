5dbc43a5a790747f582719c0b2963bb819a36f78
jules ntizimira <ntizimijules@gmail.com>
2023-06-14 22:15:47 +0000

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    _list = []
+    for i in a_dictionary:
+        if a_dictionary.get(i) == value:
+            _list.append(i)
+    for i in _list:
+        a_dictionary.pop(i) 
+    return a_dictionary
