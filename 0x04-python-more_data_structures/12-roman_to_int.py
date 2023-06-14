30efbe0df51077d7a859732343645ac9cb08b895
jules ntizimira <ntizimijules@gmail.com>
2023-06-14 20:53:35 +0000

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    dict1 = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    for i in range(-1, (len(roman_string) * -1) - 1, -1):
+        if dict1[roman_string[i]] < dict1[roman_string[i + 1]] and i != -1:
+            sum -= dict1[roman_string[i]]
+        else:
+            sum += dict1[roman_string[i]]
+    return sum
