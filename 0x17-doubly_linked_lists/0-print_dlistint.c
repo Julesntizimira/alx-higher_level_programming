87320d0c79c9c3f7a57284daa2ba17dcbb5c978f
jules ntizimira <ntizimijules@gmail.com>
2023-06-15 09:54:10 +0000

    get_dnodeint_at_index

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,9 +6,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *tmp = h;
+	const dlistint_t *tmp = NULL;
 	size_t count = 0;
-
+	if (h == NULL)
+		return (0);
+	tmp = h;
 	if (tmp->prev != NULL)
 	{
 		while (tmp->prev != NULL)
