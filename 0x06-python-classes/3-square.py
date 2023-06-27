24295e9614c9b561d2f1c1d5f9181c17f49c7d78
Julesntizimira <ntizimijules5@gmail.com>
2023-06-27 18:38:23 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Define a squre class'''
+
+
+class Square:
+    '''class square that defines a square'''
+    def __init__(self,  size=0):
+        '''constructor'''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        '''Public instance method'''
+        return self.__size ** 2
