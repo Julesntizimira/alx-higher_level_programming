f86707880565e7bf91ab269e4d324437e58e71a0
Julesntizimira <ntizimijules5@gmail.com>
2023-06-27 21:17:18 +0000

    fix pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,14 +27,16 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property  
+    @property
     def position(self):
         """Get/set square position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or type(value[0]) != int or type(value[1]) != int or value[0] < 0 or value[1] < 0:
+        if not isinstance(value, tuple) or len(value) != 2 or
+        type(value[0]) != int or type(value[1]) != int or
+        value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
