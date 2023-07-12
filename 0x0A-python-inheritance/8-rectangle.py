fa528c5f263c1c922acea291d88a0e8bad381e4d
Julesntizimira <ntizimijules5@gmail.com>
2023-07-12 16:14:37 +0000

    8th task

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' module 5-base_geometry
+    defines a class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    ''' class BaseGeometry
+    '''
+    def area(self):
+        '''raises an Exception with
+           the message area() is not implemented
+        '''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''  validates value '''
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    ''' class Rectangle that inherits to BaseGeometry
+    '''
+    def __init__(self, width, height):
+        '''constructor'''
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", self.__height)
+        self.integer_validator("width", self.__width)
