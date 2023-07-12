d39d45e76a60b9d76ddf973e52bece86a661bd80
Julesntizimira <ntizimijules5@gmail.com>
2023-07-12 17:21:39 +0000

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' defines Square class that inherit from triangle
+'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' inherit from class Rectangle
+    '''
+    def __init__(self, size):
+        '''constructor'''
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        '''returns square area '''
+        return self.__size * self.__size
+
+    def __str__(self):
+        ''' return string representation of square'''
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
