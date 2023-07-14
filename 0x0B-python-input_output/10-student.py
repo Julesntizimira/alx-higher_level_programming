987b1141b9570d53de75c47b8a463c22b58f8842
Julesntizimira <ntizimijules5@gmail.com>
2023-07-14 20:34:08 +0000

    7th task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,21 +3,20 @@
 
 
 class Student:
-    '''Represent a student.'''
+    """
+    class Student that defines a student
+    """
+
     def __init__(self, first_name, last_name, age):
-        ''' initialise a new insatance '''
+        ''' init methode or constructor '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        '''retrieves a dictionary
-           representation of a Student instance
-
-           If attrs is a list of strings, only attribute
-           names contained in this list must be retrieved.
-           Otherwise, all attributes must be retrieved
-        '''
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
         dic = {}
         if type(attrs) is list:
             for element in attrs:
