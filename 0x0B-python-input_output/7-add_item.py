9f8d883da13f1f00b29f13ec19a7a9d54dde5446
Julesntizimira <ntizimijules5@gmail.com>
2023-07-14 21:24:48 +0000

    7th task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
-from typing import List
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-def add_items_to_list(args: List[str]) -> List[str]:
     try:
-        existing_items = load_from_json_file('add_item.json')
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        existing_items = []
-
-    updated_items = existing_items + args
-    save_to_json_file(updated_items, 'add_item.json')
-
-    return updated_items
-
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    added_items = add_items_to_list(args)
-    print(f"Items added: {', '.join(added_items)}")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
