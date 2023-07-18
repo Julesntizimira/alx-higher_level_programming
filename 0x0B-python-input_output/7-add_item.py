d450e375f7421269012382bb6001164d8c86938b
Julesntizimira <ntizimijules5@gmail.com>
2023-07-18 07:52:49 +0000

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-my_list = []
+try:
+    my_list = load_from_json_file('add_item.json')
+except Exception:
+    my_list = []
+
 length = len(argv)
 j = 0
 for i in range(length):
