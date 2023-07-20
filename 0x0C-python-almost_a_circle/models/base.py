2a6f0de076b5b0e20eb10ba1db175f01a9ad960e
Julesntizimira <ntizimijules5@gmail.com>
2023-07-20 19:29:55 +0000

    update create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,9 +53,13 @@ class Base:
         '''that returns an instance
            with all attributes already set
         '''
-        A = cls(5, 2, 4, 5)
-        A.update(**dictionary)
-        return A
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
 
     @classmethod
     def load_from_file(cls):
