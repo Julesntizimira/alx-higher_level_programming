a0fc21f247935efbccd7e768e8d5c36e0bc2f97a
Julesntizimira <ntizimijules5@gmail.com>
2023-09-11 19:49:08 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * a script that prints 3 lines
+ * by using an array of string and a loop
+ */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
