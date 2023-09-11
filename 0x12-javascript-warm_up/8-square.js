8c14b0956416212d7d5dd3fce872530d6aa22f25
Julesntizimira <ntizimijules5@gmail.com>
2023-09-11 20:21:52 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+ * a script that prints x times "C is fun"
+ */
+const argv = process.argv;
+const myVar = parseInt(argv[2]);
+let str = '';
+if (isNaN(myVar)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < myVar; i++) {
+    for (let j = 0; j < myVar; j++) {
+      str += 'X';
+    }
+    str += '\n';
+  }
+  console.log(str);
+}
