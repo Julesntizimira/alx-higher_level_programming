d414bcf4db0856de0e9d68ac7e3d384ccfb3e03d
Julesntizimira <ntizimijules5@gmail.com>
2023-09-11 20:31:18 +0000

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * a script that prints the addition of 2 integers
+ */
+const argv = process.argv;
+function add(a, b) {
+  return a + b;
+}
+console.log(add(parseInt(argv[2]), parseInt(argv[3])));
