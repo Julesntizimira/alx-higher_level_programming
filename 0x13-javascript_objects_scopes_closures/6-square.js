db9074dfc1aa3d331bc96be0d1cabe246b2a3670
Julesntizimira <ntizimijules5@gmail.com>
2023-09-12 16:28:11 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/*
+ * a class Square that defines a square and inherits from Square of 5-square.js
+ */
+const square = require('./5-square');
+
+class Square extends square {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let str = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        str += c;
+      }
+      if (i !== this.height - 1) {
+        str += '\n';
+      }
+    }
+    console.log(str);
+  }
+}
+module.exports = Square;
