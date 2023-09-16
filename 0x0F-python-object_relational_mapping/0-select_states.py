53971ce65eda44f56303a7c4fab858334fc27f75
Julesntizimira <ntizimijules5@gmail.com>
2023-09-16 08:01:06 +0000

    initial

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(
             host='localhost',
             port=3306,
-            user= argv[1],
+            user=argv[1],
             password=argv[2],
             db=argv[3]
             )
